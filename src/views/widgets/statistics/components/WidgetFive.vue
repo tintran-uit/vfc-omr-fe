8be36531169c3a59e291d9d50d404b64400e5a13
Hải Văn Việt <vanviethai89@gmail.com>
2025-05-24 23:46:00 +0700

    Add charts demo

--- src/views/widgets/statistics/components/WidgetFive.vue
+++ src/views/widgets/statistics/components/WidgetFive.vue
@@ -2,40 +2,40 @@
 import { shallowRef } from 'vue';
 
 // icons
-import { RiseOutlined, FallOutlined } from '@ant-design/icons-vue';
+import { RiseOutlined, FallOutlined, BankOutlined, UsergroupAddOutlined, MoneyCollectFilled } from '@ant-design/icons-vue';
 
 const fivecards = shallowRef([
   {
-    name: 'Total Page Views',
-    earn: '4,42,236',
-    percent: '59.3%',
+    name: 'Church Plants',
+    earn: '120',
+    percent: null,
+    color: 'primary',
+    icon: BankOutlined,
+    text: null
+  },
+  {
+    name: 'People',
+    earn: '1,500',
+    percent: '',
+    color: 'primary',
+    icon: UsergroupAddOutlined,
+    text: null
+  },
+  {
+    name: 'Giving',
+    earn: '50,000',
+    percent: null,
+    color: 'primary',
+    icon: MoneyCollectFilled,
+    text: null
+  },
+  {
+    name: 'Growth',
+    earn: '$35,078',
+    percent: '4%',
     color: 'primary',
     icon: RiseOutlined,
-    text: '35,000'
-  },
-  {
-    name: 'Total Users',
-    earn: '78,250',
-    percent: '70.5%',
-    color: 'success',
-    icon: RiseOutlined,
-    text: '8,900'
-  },
-  {
-    name: 'Total Order',
-    earn: '18,800',
-    percent: '27.4%',
-    color: 'warning',
-    icon: FallOutlined,
-    text: '1,943'
-  },
-  {
-    name: 'Total Sales',
-    earn: '$35,078',
-    percent: '27.4%',
-    color: 'error',
-    icon: FallOutlined,
-    text: '$20,395'
+    text: null
   }
 ]);
 </script>
@@ -46,21 +46,21 @@ const fivecards = shallowRef([
         <v-card variant="outlined" class="h-100">
           <v-card-text class="h-100">
             <div class="d-flex align-items-center justify-space-between">
-              <div>
-                <h6 class="text-h6 text-lightText mb-1">{{ card5.name }}</h6>
-                <h4 class="text-h4 d-flex align-center mb-0">
-                  {{ card5.earn }}
-                  <v-chip :color="card5.color" :border="`${card5.color} solid thin opacity-50`" class="ms-2" size="small" label>
-                    <template v-slot:prepend>
-                      <component :is="card5.icon" :style="{ fontSize: '12px' }" :class="'me-1 text-' + card5.color" />
-                    </template>
-                    {{ card5.percent }}
-                  </v-chip>
-                </h4>
-                <span class="text-lightText text-caption pt-5 d-block"
-                  >You made an extra <span :class="'text-' + card5.color">{{ card5.text }}</span> this year</span
-                >
-              </div>
+              <v-row class="mb-0">
+                <v-col cols="3">
+                  <component :is="card5.icon" :style="{ fontSize: '25px', verticalAlign: 'middle' }" :class="'me-1 text-' + card5.color" />
+                </v-col>
+
+                <v-col cols="7">
+                    <h6 class="text-h6 text-lightText mb-1">{{ card5.name }}</h6>
+                    <h4 class="text-h4 d-flex align-center mb-0">
+                      {{ card5.earn }}
+                    </h4>
+                    <!-- <span class="text-lightText text-caption pt-5 d-block"
+                      >You made an extra <span :class="'text-' + card5.color">{{ card5.text }}</span> this year</span
+                    > -->
+                </v-col>
+              </v-row>
             </div>
           </v-card-text>
         </v-card>
